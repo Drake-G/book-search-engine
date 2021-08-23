8da42f540892ab986b78d511f3dd29a7d6b8acae
Drake Gooding <dgooding1008@gmail.com>
2021-08-23 15:19:21 -0500

    Started login and remove book mutations

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -15,5 +15,29 @@ Mutation: {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
-    }
+    },
+    login: async (parent, { email, password }) => {
+        const user = await User.findOne({ email });
+  
+        if (!user) {
+          throw new AuthenticationError('No user found with this email address');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
+      },
+      removeBook: async (parent, { bookId }, context) => {
+          if(context.user) {
+              const userList = await User.findOneandDelete({
+                  _id: bookId
+              })
+          }
+      }
 }
