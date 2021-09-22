05d0bdb1757d23fddf149467c7b52ce09658a324
Drake Gooding <dgooding1008@gmail.com>
2021-09-22 00:21:18 -0500

    removing comments from auth.js

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -7,10 +7,7 @@ const expiration = '2h';
 module.exports = {
   // function for our authenticated routes
   authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
     let token = req.query.token || req.body.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -19,7 +16,6 @@ module.exports = {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -27,8 +23,6 @@ module.exports = {
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
-
-    // send to next endpoint
     return req;
   },
   signToken: function ({ username, email, _id }) {
